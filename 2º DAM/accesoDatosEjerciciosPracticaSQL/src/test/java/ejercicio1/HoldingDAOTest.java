cd69bc8a429fd8c7e57c6e486d1d122bc534ec05
Jose <garcianavarro.jmiguel@gmail.com>
2023-11-10 19:04:49 +0100

    2023/11/10

--- 2º DAM/accesoDatosEjerciciosPracticaSQL/src/test/java/ejercicio1/HoldingDAOTest.java
+++ 2º DAM/accesoDatosEjerciciosPracticaSQL/src/test/java/ejercicio1/HoldingDAOTest.java
@@ -23,6 +23,7 @@ class HoldingDAOTest {
     @Test
     void agregarEmpleado() {
         dao.agregarEmpleado("Jose","García","1986-01-01","Desarrollador", "josegarcia@gmail.com", "2024-07-15",1900.0,"CodeCrafters");
+
 }
 
     @Test
@@ -57,11 +58,11 @@ class HoldingDAOTest {
 
     @Test
     void resumenProyectos() {
-        String esperado = "CodeFusion María 6300.0\n" +
-                "CodeFusion Juan 6300.0\n" +
-                "CyberPulse Laura 3800.0\n" +
-                "FusionWorks Carlos 4500.0\n" +
-                "QuantumQuest null 0.0\n";
+        String esperado = "CodeFusion Juan 6300.0\n" +
+                          "CodeFusion María 6300.0\n" +
+                          "CyberPulse Laura 3800.0\n" +
+                          "FusionWorks Carlos 4500.0\n" +
+                          "QuantumQuest null 0.0\n";
         String actual = dao.resumenProyectos();
         assertEquals(esperado, actual);
     }
