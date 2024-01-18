6422b46deff77980e5b539de3ee63d8b44f376d7
Jose <garcianavarro.jmiguel@gmail.com>
2024-01-18 20:08:52 +0100

    2024/01/18

--- 2º DAM/procesosLanzaHilos/src/main/java/org/example/Hilo.java
+++ 2º DAM/procesosLanzaHilos/src/main/java/org/example/Hilo.java
@@ -1,17 +1,21 @@
 package org.example;
 
-public class Hilo implements Runnable{
+public class Hilo implements Runnable {
     private final String nombre;
+
+    // Constructor que recibe el nombre del hilo
     public Hilo(String nombre) {
         this.nombre = nombre;
     }
+
+    // Método getter para obtener el nombre del hilo
     public String getNombre() {
         return nombre;
     }
 
+    // Método run() que se ejecuta cuando se inicia el hilo
     @Override
-    public void run(){
-
+    public void run() {
         System.out.printf("Hola soy el hilo: %s.\n", this.nombre);
         System.out.printf("Hilo %s ha terminado.\n", this.nombre);
     }
--- 2º DAM/procesosLanzaHilos/src/main/java/org/example/Main.java
+++ 2º DAM/procesosLanzaHilos/src/main/java/org/example/Main.java
@@ -4,14 +4,16 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        // Se crean dos hilos con instancias de la clase Hilo
         Thread h1 = new Thread(new Hilo("H1"));
         Thread h2 = new Thread(new Hilo("H2"));
 
+        // Se inician los hilos
         h1.start();
         h2.start();
 
-        //Hilo main espera 1 seg
-        TimeUnit.MICROSECONDS.sleep(2500);
+        // El hilo principal espera 1.5 segundos
+        TimeUnit.MILLISECONDS.sleep(1500);
         System.out.println("Hilo principal terminado.");
     }
 }
